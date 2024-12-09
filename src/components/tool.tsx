a07354b1237cbd841942db4e63017011dbaab915
Dustin Rouillard <code@dstn.to>
2024-12-09 04:16:16 -0700

    Fixing home page icon aspect ratios

--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -17,13 +17,13 @@ export function Tool({ tool, runeToolRandom }: { tool: ITool } & Props) {
     <Link href={tool.path}>
       <div
         style={{ borderBottomColor: tool.color }}
-        className={`flex flex-row h-40 justify-between rounded-lg m-1 bg-neutral-200 dark:bg-black/60 p-6 min-w-80 border-b-8 space-x-6 cursor-pointer hover:brightness-75 transition-all`}
+        className={`flex flex-row h-40 justify-between items-center rounded-lg m-1 bg-neutral-200 dark:bg-black/60 p-6 min-w-80 border-b-8 space-x-6 cursor-pointer hover:brightness-75 transition-all`}
       >
         <div className="flex flex-col space-y-2">
           <h3 className="text-lg font-bold line-clamp-1">{tool.name}</h3>
           <p className="text-wrap line-clamp-2">{tool.description}</p>
         </div>
-        <div className="flex flex-row w-36 p-1">
+        <div className="flex flex-row w-36 h-fit items-center justify-center p-1">
           {tool.icon ? tool.icon(runeToolRandom) : <></>}
         </div>
       </div>
